680e4f346f6bf23a6fe7f9a4abd2595dabbfc107
Leyla Becker <git@jan-leila.com>
2022-05-29 21:37:57 -0500

    cleaned up things a bit

--- src/main/java/me/wurgo/antiresourcereload/mixin/MinecraftClientMixin.java
+++ src/main/java/me/wurgo/antiresourcereload/mixin/MinecraftClientMixin.java
@@ -1,17 +1,14 @@
 package me.wurgo.antiresourcereload.mixin;
 
-import me.wurgo.antiresourcereload.AntiResourceReload;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.world.ClientWorld;
 import net.minecraft.resource.*;
 import net.minecraft.server.command.CommandManager;
-import org.jetbrains.annotations.Nullable;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -19,22 +16,31 @@ import java.util.concurrent.Executor;
 
 @Mixin(MinecraftClient.class)
 public abstract class MinecraftClientMixin {
-    @Redirect(method = "method_29604", at = @At(value = "INVOKE", target = "Lnet/minecraft/resource/ServerResourceManager;reload(Ljava/util/List;Lnet/minecraft/server/command/CommandManager$RegistrationEnvironment;ILjava/util/concurrent/Executor;Ljava/util/concurrent/Executor;)Ljava/util/concurrent/CompletableFuture;"))
-    private CompletableFuture<ServerResourceManager> removeReload(List<ResourcePack> dataPacks, CommandManager.RegistrationEnvironment registrationEnvironment, int i, Executor executor, Executor executor2) {
-        CompletableFuture<ServerResourceManager> manager = AntiResourceReload.manager;
 
-        if (manager != null && dataPacks.size() == 1) {
-            AntiResourceReload.log("Using cached server resources.");
-            return manager;
-        }
-        else if (dataPacks.size() > 1) { AntiResourceReload.log("Using data-packs, reloading."); }
-        else { AntiResourceReload.log("Cached resources unavailable, reloading."); }
+    private static CompletableFuture<ServerResourceManager> managerProvider;
 
-        CompletableFuture<ServerResourceManager> future = ServerResourceManager.reload(dataPacks, registrationEnvironment, i, executor, executor2);
-        if (dataPacks.size() == 1) {
-            AntiResourceReload.manager = future;
-        }
-
-        return future;
+    private static final Logger LOGGER = LogManager.getLogger(FabricLoader.getInstance().getModContainer("antiresourcereload").get().getMetadata().getName());
+    private static void log(String message) {
+        LOGGER.info(message);
     }
+
+    @Redirect(method = "method_29604", at = @At(value = "INVOKE", target = "Lnet/minecraft/resource/ServerResourceManager;reload(Ljava/util/List;Lnet/minecraft/server/command/CommandManager$RegistrationEnvironment;ILjava/util/concurrent/Executor;Ljava/util/concurrent/Executor;)Ljava/util/concurrent/CompletableFuture;"))
+    private CompletableFuture<ServerResourceManager> cachedReload(List<ResourcePack> dataPacks, CommandManager.RegistrationEnvironment registrationEnvironment, int i, Executor executor, Executor executor2) {
+
+        if(managerProvider == null){
+            log("loading server resources.");
+        }
+        else if(dataPacks.size() != 1){
+            log("reloading server resources.");
+        }
+        else {
+            log("using cached server resources.");
+            return managerProvider;
+        }
+
+        managerProvider = ServerResourceManager.reload(dataPacks, registrationEnvironment, i, executor, executor2);
+
+        return managerProvider;
+    }
+
 }
