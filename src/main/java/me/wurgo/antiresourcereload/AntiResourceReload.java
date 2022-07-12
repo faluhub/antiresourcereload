56b47ab0b976a3a75f34563028fb3ea1d14d8253
wurgo <laxvideoeffects@gmail.com>
2022-07-12 20:33:02 +0100

    1.14.4

--- src/main/java/me/wurgo/antiresourcereload/AntiResourceReload.java
+++ src/main/java/me/wurgo/antiresourcereload/AntiResourceReload.java
@@ -1,20 +1,29 @@
 package me.wurgo.antiresourcereload;
 
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.loot.LootManager;
 import net.minecraft.recipe.RecipeManager;
 import net.minecraft.resource.ReloadableResourceManagerImpl;
 import net.minecraft.server.ServerAdvancementLoader;
 import net.minecraft.server.function.CommandFunctionManager;
 import net.minecraft.tag.RegistryTagManager;
+import net.minecraft.util.Unit;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class AntiResourceReload {
+import java.util.concurrent.CompletableFuture;
 
-    public static ReloadableResourceManagerImpl dataManager;
-    //public static ResourcePackManager<ResourcePackProfile> dataPackManager;
-    public static RecipeManager recipeManager;
-    public static RegistryTagManager tagManager;
-    public static LootManager lootManager;
-    public static ServerAdvancementLoader advancementLoader;
-    public static CommandFunctionManager commandFunctionManager;
+public class AntiResourceReload implements ModInitializer {
+    public static CompletableFuture<Unit> managerProvider;
+    private static final Logger LOGGER = LogManager.getLogger(FabricLoader.getInstance().getModContainer("antiresourcereload").get().getMetadata().getName());
 
+    public static void log(String message) {
+        LOGGER.info("[AntiResourceReload] " + message);
+    }
+
+    @Override
+    public void onInitialize() {
+        log("Initializing.");
+    }
 }
--- src/main/java/me/wurgo/antiresourcereload/mixin/MinecraftServerMixin.java
+++ src/main/java/me/wurgo/antiresourcereload/mixin/MinecraftServerMixin.java
@@ -8,67 +8,31 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.ServerAdvancementLoader;
 import net.minecraft.server.function.CommandFunctionManager;
 import net.minecraft.tag.RegistryTagManager;
-import org.apache.logging.log4j.Logger;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.util.Unit;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
 @Mixin(MinecraftServer.class)
 public abstract class MinecraftServerMixin {
-    @Shadow @Final private static Logger LOGGER;
-    
-    private static boolean register = true;
-
-    @Redirect(method = "<init>", at = @At(value = "NEW", target = "net/minecraft/resource/ReloadableResourceManagerImpl"))
-    private ReloadableResourceManagerImpl skipReloadingDataManager(ResourceType type, Thread mainThread) {
-        if (AntiResourceReload.dataManager != null) {
-            LOGGER.info("Using cached server resources.");
-            register = false;
-            return AntiResourceReload.dataManager;
-        } else {
-            LOGGER.info("Cached resources unavailable, reloading.");
-            register = true;
-            return (AntiResourceReload.dataManager = new ReloadableResourceManagerImpl(type, mainThread));
+    @Redirect(method = "reloadDataPacks", at = @At(value = "INVOKE", target = "Lnet/minecraft/resource/ReloadableResourceManager;beginReload(Ljava/util/concurrent/Executor;Ljava/util/concurrent/Executor;Ljava/util/List;Ljava/util/concurrent/CompletableFuture;)Ljava/util/concurrent/CompletableFuture;"))
+    private CompletableFuture<Unit> antiresourcereload_cachedReload(ReloadableResourceManager instance, Executor executor, Executor executor2, List<ResourcePack> dataPacks, CompletableFuture<Unit> unitCompletableFuture) {
+        if (dataPacks.size() != 1) { AntiResourceReload.log("Using data-packs, reloading."); }
+        else if (AntiResourceReload.managerProvider == null) { AntiResourceReload.log("Cached resources unavailable, reloading & caching."); }
+        else {
+            AntiResourceReload.log("Using cached server resources.");
+            return AntiResourceReload.managerProvider;
         }
+
+        CompletableFuture<Unit> reloaded = instance.beginReload(executor, executor2, dataPacks, unitCompletableFuture);
+
+        if (dataPacks.size() == 1) { AntiResourceReload.managerProvider = reloaded; }
+
+        return reloaded;
     }
 
-//    Not caching DataPackManager because it can cause issues when using/switching Datapacks
 
-//    @Redirect(method = "<init>", at = @At(value = "NEW", target = "net/minecraft/resource/ResourcePackManager"))
-//    private ResourcePackManager skipReloadingDataPackManager(ResourcePackProfile.Factory factory) {
-//        return AntiResourceReload.dataPackManager == null ? (AntiResourceReload.dataPackManager = new ResourcePackManager(factory)) : AntiResourceReload.dataPackManager;
-//    }
-
-    @Redirect(method = "<init>", at = @At(value = "NEW", target = "net/minecraft/recipe/RecipeManager"))
-    private RecipeManager skipReloadingRecipeManager() {
-        return AntiResourceReload.recipeManager == null ? (AntiResourceReload.recipeManager = new RecipeManager()) : AntiResourceReload.recipeManager;
-    }
-
-    @Redirect(method = "<init>", at = @At(value = "NEW", target = "net/minecraft/tag/RegistryTagManager"))
-    private RegistryTagManager skipReloadingTagManager() {
-        return AntiResourceReload.tagManager == null ? (AntiResourceReload.tagManager = new RegistryTagManager()) : AntiResourceReload.tagManager;
-    }
-
-    @Redirect(method = "<init>", at = @At(value = "NEW", target = "net/minecraft/loot/LootManager"))
-    private LootManager skipReloadingLootManager() {
-        return AntiResourceReload.lootManager == null ? (AntiResourceReload.lootManager = new LootManager()) : AntiResourceReload.lootManager;
-    }
-
-    @Redirect(method = "<init>", at = @At(value = "NEW", target = "net/minecraft/server/ServerAdvancementLoader"))
-    private ServerAdvancementLoader skipReloadingAdvancementManager() {
-        return AntiResourceReload.advancementLoader == null ? (AntiResourceReload.advancementLoader = new ServerAdvancementLoader()) : AntiResourceReload.advancementLoader;
-    }
-
-    @Redirect(method = "<init>", at = @At(value = "NEW", target = "net/minecraft/server/function/CommandFunctionManager"))
-    private CommandFunctionManager skipReloadingCommandFunctionManager(MinecraftServer server) {
-        return AntiResourceReload.commandFunctionManager == null ? (AntiResourceReload.commandFunctionManager = new CommandFunctionManager(server)) : AntiResourceReload.commandFunctionManager;
-    }
-
-    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/resource/ReloadableResourceManager;registerListener(Lnet/minecraft/resource/ResourceReloadListener;)V"))
-    private void register(ReloadableResourceManager instance, ResourceReloadListener resourceReloadListener){
-        if (register) {
-            instance.registerListener(resourceReloadListener);
-        }
-    }
 }
