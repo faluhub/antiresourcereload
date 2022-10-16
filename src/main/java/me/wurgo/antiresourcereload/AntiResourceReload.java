f2be7c03d00f828291ea6149f57f89de527487f8
KingContaria <98704937+KingContaria@users.noreply.github.com>
2022-10-16 15:11:35 +0200

    two fixes
    fix wrong recipes being cached
    fix non-cached resourcemanagers not being closed

--- src/main/java/me/wurgo/antiresourcereload/AntiResourceReload.java
+++ src/main/java/me/wurgo/antiresourcereload/AntiResourceReload.java
@@ -3,11 +3,13 @@ package me.wurgo.antiresourcereload;
 import com.google.gson.JsonElement;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.resource.ServerResourceManager;
 import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 public class AntiResourceReload implements ModInitializer {
     private static final Logger LOGGER = LogManager.getLogger(FabricLoader.getInstance().getModContainer("antiresourcereload").get().getMetadata().getName());
@@ -16,6 +18,7 @@ public class AntiResourceReload implements ModInitializer {
         LOGGER.info("[AntiResourceReload] " + message);
     }
 
+    public static CompletableFuture<ServerResourceManager> cache;
     public static Map<Identifier, JsonElement> recipes;
     public static boolean hasSeenRecipes;
 
