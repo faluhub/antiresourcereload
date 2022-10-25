90aebc15ebae09d21074e5633cba02b87ea08837
KingContaria <98704937+KingContaria@users.noreply.github.com>
2022-10-25 15:27:33 +0200

    small fix

--- src/main/java/me/wurgo/antiresourcereload/mixin/ResourceManagerHolderMixin.java
+++ src/main/java/me/wurgo/antiresourcereload/mixin/ResourceManagerHolderMixin.java
@@ -2,8 +2,11 @@ package me.wurgo.antiresourcereload.mixin;
 
 import me.wurgo.antiresourcereload.AntiResourceReload;
 import net.minecraft.resource.LifecycledResourceManager;
+import net.minecraft.server.DataPackContents;
 import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
@@ -11,10 +14,12 @@ import java.util.concurrent.ExecutionException;
 
 @Mixin(MinecraftServer.ResourceManagerHolder.class)
 public class ResourceManagerHolderMixin {
+
+    @Shadow @Final DataPackContents dataPackContents;
+
     @Redirect(method = "close", at = @At(value = "INVOKE", target = "Lnet/minecraft/resource/LifecycledResourceManager;close()V"))
     private void antiresourcereload_keepOpened(LifecycledResourceManager instance) throws ExecutionException, InterruptedException {
-        //noinspection ConstantConditions - I think mixin is confusing for intellij here
-        if (AntiResourceReload.cache == null || (Object) this != AntiResourceReload.cache.get()) {
+        if (AntiResourceReload.cache == null || this.dataPackContents != AntiResourceReload.cache.get()) {
             instance.close();
         }
     }
